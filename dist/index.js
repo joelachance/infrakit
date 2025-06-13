ae89167409b6d23678ec94d067fd502993920d5c
joe <jlachance1@gmail.com>
2025-06-13 14:38:55 -0500

    Ignoring dist.

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./log");
-function main() {
-    console.log("Hello, world!");
-    // throw new Error("Boom");
-}
-main();
